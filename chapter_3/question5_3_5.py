633bf1ba52ea00d3d6f3222016c420c6d556f075
Vera Ezeagu <veraeze18@gmail.com>
2023-10-05 11:56:34 +0100

    removeduplicateupdate

---
+++ chapter_3/question5_3_5.py
@@ -0,0 +1,8 @@
+x = ['A', 'M', 'C', 'W', 'I', 'T']
+n = len(x) / 2
+for letter in x:
+    x.append(letter)
+print(x)
+
+
+
--- list_exercise/remove_duplicate.py
+++ list_exercise/remove_duplicate.py
@@ -1,13 +1,47 @@
 numbers = [15, 20, 25, 20, 10, 5]
-high = 0
-duplicate = 0
-for number in numbers:
-    count = 0
-    for integer in numbers:
-        if number == integer:
-            count += 1
-    if count > high:
-        high = count
-        duplicate = number
-numbers.remove(duplicate)
-print(numbers)
+new = []
+for number in range(len(numbers)):
+    if numbers[number] not in new:
+        new.append(numbers[number])
+print(new)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# high = 0
+# duplicate = 0
+# for number in numbers:
+#     count = 0
+#     for integer in numbers:
+#         if number == integer:
+#             count += 1
+#     if count > high:
+#         high = count
+#         duplicate = number
+# numbers.remove(duplicate)
+# print(numbers)
